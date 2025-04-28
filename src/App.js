6d0858a0c12e5e8841a851f9fd9e8e0ad09054c3
Israq <ragib.israq9833@gmail.com>
2025-04-28 08:51:00 +0600

    added modal and continue

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import "./App.css";
 import EmployeeNot from "./components/EmployeeNot";
 import Employee from "./components/Emplyee";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 function App() {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Caleb",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/10171447/pexels-photo-10171447.png",
+    },
+    {
+      name: "John",
+      role: "System Architect",
+      img: "https://images.pexels.com/photos/10131167/pexels-photo-10131167.jpeg",
+    },
+    {
+      name: "Melanie",
+      role: "Sr. Developer",
+      img: "https://images.pexels.com/photos/3762952/pexels-photo-3762952.jpeg",
+    },
+    {
+      name: "Richard",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/13758252/pexels-photo-13758252.jpeg",
+    },
+  ]);
   const showEmplyees = true;
   return (
     <div className="App">
@@ -16,37 +39,18 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <div class="flex flex-wrap">
-            <Employee
-              name="Israq"
-              role="Soft Engineer"
-              img="https://images.pexels.com/photos/10131167/pexels-photo-10131167.jpeg"
-            />
-            <Employee
-              name="John"
-              role={role}
-              img="https://images.pexels.com/photos/2072456/pexels-photo-2072456.jpeg"
-            />
-            <Employee
-              name="Israq"
-              role="Soft Engineer"
-              img="https://images.pexels.com/photos/10131167/pexels-photo-10131167.jpeg"
-            />
-            <Employee
-              name="John"
-              role={role}
-              img="https://images.pexels.com/photos/2072456/pexels-photo-2072456.jpeg"
-            />
-            <Employee
-              name="Israq"
-              role="Soft Engineer"
-              img="https://images.pexels.com/photos/10131167/pexels-photo-10131167.jpeg"
-            />
-            <Employee
-              name="John"
-              role={role}
-              img="https://images.pexels.com/photos/2072456/pexels-photo-2072456.jpeg"
-            />
+          <div class="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              console.log(uuidv4());
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
